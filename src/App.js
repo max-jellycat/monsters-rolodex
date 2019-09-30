85b3eac7871f307cbd60fa2384889e648d528efe
max-jellycat <max@jellycat.fr>
2019-10-01 00:55:43 +0200

    architecture and setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    monsters: []
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }))
+      .catch(err => console.error(err))
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        {this.state.monsters.map(monster => (
+          <h1 key={monster.id}>{monster.username}</h1>
+        ))}
+      </div>
+    )
+  }
 }
 
 export default App;
