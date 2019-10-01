8db3f51d54009171950728aa3ae4e8ff355bd117
max-jellycat <max@jellycat.fr>
2019-10-01 02:31:01 +0200

    Updates

---
+++ precache-manifest.afc4e8b85f42e7c1528b2a5495ff75a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8e11d62dffaf18478f33fd1410ad6d6",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "7a63f6b3f352537ec85b",
+    "url": "/monsters-rolodex/static/css/main.f914fd59.chunk.css"
+  },
+  {
+    "revision": "4ee53b84b1f31b69fcd8",
+    "url": "/monsters-rolodex/static/js/2.01c3c59a.chunk.js"
+  },
+  {
+    "revision": "7a63f6b3f352537ec85b",
+    "url": "/monsters-rolodex/static/js/main.5ccdfa46.chunk.js"
+  },
+  {
+    "revision": "61e64e548c9076aea34b",
+    "url": "/monsters-rolodex/static/js/runtime-main.c620570a.js"
+  }
+]);
